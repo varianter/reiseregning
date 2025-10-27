c34df03334e45cc9a4deed3d6283ee40751dc8b8
oddsve <odd.morten@sveas.org>
2025-10-27 10:12:49 +0100

    legger til en tooltip som forklarer når man skal bruke diett

--- src/Components/Refund.js
+++ src/Components/Refund.js
@@ -60,6 +60,13 @@ class Refund extends React.Component {
                 onChange={this.props.handleUsePerDiemChange}
             />
             Bruk diettberegning
+            <span className="tooltip-container">?
+              <span className="tooltip-text">
+                <p>Denne brukes bare <strong>untaksvis</strong>. Ved Variant-interne reiser brukes <strong>ikke</strong> diett. 
+                For kundereiser har vi ofte diett, men da skal vi fortrinnsvis bruke kundens reiseregningsskjema. </p>
+                <p>Untaktsvis har ikke kunde eget skjema eller ønsker ikke å bruke dette, og da kan denne seksjonen benyttes. </p>
+              </span>
+            </span>
           </label>
         </h2>
         {this.props.usePerDiem && (
